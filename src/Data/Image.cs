25525846d94418ad61a9484a3a29e6c819c396d8
Adrian <hyiev1@users.noreply.github.com>
2026-04-02 19:38:12 +0200

    Enhance Image class

--- src/Data/Image.cs
+++ src/Data/Image.cs
@@ -1,8 +1,26 @@
 namespace tara_tool.Data;
+using System.Security.Cryptography;
 
 public class Image
 {
-  public long Id { get; set; }
-  public byte[]? Data { get; set; } = null;
-  public byte[]? Hash { get; set; } = null;
+  public required long Id { get; set; }
+  private byte[] _data;
+
+  public byte[] Data
+  {
+    get => _data;
+    set
+    {
+      _data = value ?? throw new ArgumentNullException(nameof(Data));
+      // Recompute the Hash every time the image data changes 
+      Hash = ComputeHash(_data);
+    }
+  }
+
+  public byte[] Hash { get; private set; }
+
+  private byte[] ComputeHash(byte[] data)
+  {
+    return SHA256.Create().ComputeHash(data);
+  }
 }
