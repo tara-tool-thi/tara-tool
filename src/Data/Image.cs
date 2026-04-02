ce840f6ebe1c5f885f94532c61b0a5942a77a61c
ardwetha <boeckchensemail@gmx.de>
2026-04-02 22:13:40 +0200

    Workaround for CS8618 Compiler Warning
    I also added some attribution to hyiev1, because I am unsure, if the
    commit messages to this branch will be stored, due to them beeing
    squashed before merging into main.

--- src/Data/Image.cs
+++ src/Data/Image.cs
@@ -1,10 +1,17 @@
 namespace tara_tool.Data;
+
 using System.Security.Cryptography;
 
 public class Image
 {
   public required long Id { get; set; }
-  private byte[] _data;
+
+  // Needed as a workaround for CS8618. See GitHub Issue:
+  // https://github.com/dotnet/roslyn/issues/32358. Not the best approach and
+  // one I really dislike tbh. - Ardwetha
+  // Thanks to hyiev1 for improving the
+  // class by adding/rewritting the behavior
+  private byte[] _data = null!;
 
   public byte[] Data
   {
@@ -12,15 +19,17 @@ public class Image
     set
     {
       _data = value ?? throw new ArgumentNullException(nameof(Data));
-      // Recompute the Hash every time the image data changes 
+      // Recompute the Hash every time the image data changes
       Hash = ComputeHash(_data);
     }
   }
 
-  public byte[] Hash { get; private set; }
+  public byte[] Hash { get; private set; } = null!;
 
   private byte[] ComputeHash(byte[] data)
   {
     return SHA256.Create().ComputeHash(data);
   }
+
+  public Image(byte[] data) { Data = data; }
 }
