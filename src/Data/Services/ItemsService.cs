5666f7b732f027ad0b715c4592770ad6b0118027
U ‎ <znaxkyydr4he@protonmail.com>
2026-04-08 17:39:30 +0200

    Added dropdown to change num of items per page; added redirect to Item page when clicking New Item Button

--- src/Data/Services/ItemsService.cs
+++ src/Data/Services/ItemsService.cs
@@ -5,43 +5,18 @@ namespace tara_tool.Data;
 public class ItemsService(ApplicationDbContext context)
 {    
     public async Task CreateItemAsync(long projectID, string name)
-    {
-        ItemDefinition newItem = new ItemDefinition
         {
-            ItemName = name,
-            Project = await context.Projects.FindAsync(projectID) 
-            ?? throw new Exception("Invalid Project ID for Item Creation")
-        };
+            ItemDefinition newItem = new ItemDefinition
+            {
+                ItemName = name,
+                Project = await context.Projects.FindAsync(projectID) 
+                ?? throw new Exception("Invalid Project ID for Item Creation")
+            };
 
             context.ItemDefinitions.Add(newItem);
             newItem.Project.DateLastChanged = DateTime.Now;
             await context.SaveChangesAsync();
 
-        return;
-    }
-
-    public async Task<ItemDefinition?> RetrieveItemDefinitionInfoAsync(long itemID)
-    {
-        ItemDefinition? itemDefinition = await context.ItemDefinitions
-            .Include(item => item.TechnicalSketch)
-            .FirstOrDefaultAsync(item => item.Id == itemID);
-
-        return itemDefinition;
-    }
-
-    public async Task SaveItemAsync(ItemDefinition item)
-    {
-        if (context.Entry(item).State == EntityState.Detached)
-        {
-            context.ItemDefinitions.Update(item);
+            return;
         }
-
-        if (item.TechnicalSketch != null &&
-            context.Entry(item.TechnicalSketch).State == EntityState.Detached)
-        {
-            context.Images.Add(item.TechnicalSketch);
-        }
-
-        await context.SaveChangesAsync();
-    }
 }
