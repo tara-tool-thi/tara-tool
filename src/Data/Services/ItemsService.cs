7785a659df96b72257cdf63368aba1068d39cb0a
Adrian <hyiev1@users.noreply.github.com>
2026-04-08 14:32:57 +0200

    Show item info from db in item definition page

--- src/Data/Services/ItemsService.cs
+++ src/Data/Services/ItemsService.cs
@@ -3,17 +3,24 @@ namespace tara_tool.Data;
 public class ItemsService(ApplicationDbContext context)
 {    
     public async Task CreateItemAsync(long projectID, string name)
+    {
+        ItemDefinition newItem = new ItemDefinition
         {
-            ItemDefinition newItem = new ItemDefinition
-            {
-                ItemName = name,
-                Project = await context.Projects.FindAsync(projectID) 
-                ?? throw new Exception("Invalid Project ID for Item Creation")
-            };
+            ItemName = name,
+            Project = await context.Projects.FindAsync(projectID) 
+            ?? throw new Exception("Invalid Project ID for Item Creation")
+        };
 
-            context.ItemDefinitions.Add(newItem);
-            await context.SaveChangesAsync();
+        context.ItemDefinitions.Add(newItem);
+        await context.SaveChangesAsync();
 
-            return;
-        }
+        return;
+    }
+
+    public async Task<ItemDefinition?> RetrieveItemDefinitionInfoAsync(long id)
+    {
+        ItemDefinition? itemDefinition = await context.ItemDefinitions.FindAsync(id);
+
+        return itemDefinition;
+    }
 }
