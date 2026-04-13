69799b112992c3c4560eed295f8d8a9a4fe72fb7
parasomni <parasomni@proton.me>
2026-04-13 17:00:34 +0200

    resolve merge conflicts

--- src/Data/Services/ItemsService.cs
+++ src/Data/Services/ItemsService.cs
@@ -4,19 +4,44 @@ namespace tara_tool.Data;
 
 public class ItemsService(ApplicationDbContext context)
 {    
-    public async Task CreateItemAsync(long projectID, string name)
+    public async Task<ItemDefinition> CreateItemAsync(long projectID, string name)
+    {
+        ItemDefinition newItem = new ItemDefinition
         {
-            ItemDefinition newItem = new ItemDefinition
-            {
-                ItemName = name,
-                Project = await context.Projects.FindAsync(projectID) 
-                ?? throw new Exception("Invalid Project ID for Item Creation")
-            };
+            ItemName = name,
+            Project = await context.Projects.FindAsync(projectID) 
+            ?? throw new Exception("Invalid Project ID for Item Creation")
+        };
 
             context.ItemDefinitions.Add(newItem);
             newItem.Project.DateLastChanged = DateTime.Now;
             await context.SaveChangesAsync();
 
-            return;
+        return newItem;
+    }
+
+    public async Task<ItemDefinition?> RetrieveItemDefinitionInfoAsync(long itemID)
+    {
+        ItemDefinition? itemDefinition = await context.ItemDefinitions
+            .Include(item => item.TechnicalSketch)
+            .FirstOrDefaultAsync(item => item.Id == itemID);
+
+        return itemDefinition;
+    }
+
+    public async Task SaveItemAsync(ItemDefinition item)
+    {
+        if (context.Entry(item).State == EntityState.Detached)
+        {
+            context.ItemDefinitions.Update(item);
         }
+
+        if (item.TechnicalSketch != null &&
+            context.Entry(item.TechnicalSketch).State == EntityState.Detached)
+        {
+            context.Images.Add(item.TechnicalSketch);
+        }
+
+        await context.SaveChangesAsync();
+    }
 }
