ccb6548baeacaab011aea2bf9a27f90c30f487d9
ardwetha <boeckchensemail@gmx.de>
2026-04-14 09:29:42 +0200

    Changed AutoComplete to Search, reqrote AccessService
    Made the access service and actuall service, by injecting it into the
    page. Removed the autocomplete and favoured a search, due to the auto
    complete only beeing able to search for one element at once. Rewrote the
    access service to be a little cleaner.

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.FluentUI.AspNetCore.Components;
 using tara_tool.Components;
 using tara_tool.Components.Account;
 using tara_tool.Data;
+using tara_tool.Data.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +33,8 @@ var connectionString =
 builder.Services.AddDbContext<ApplicationDbContext>(
     options => options.UseSqlite(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddTransient<AccessControlService>();
 
 builder.Services
     .AddIdentityCore<ApplicationUser>(options =>
