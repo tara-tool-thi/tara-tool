c8260d05f6229d16bd4a3d1e58fe25790c96eeac
ardwetha <boeckchensemail@gmx.de>
2026-04-14 10:23:51 +0200

    Updated

--- src/Data/Services/AccessControlService.cs
+++ src/Data/Services/AccessControlService.cs
@@ -7,21 +7,21 @@ public class AccessControlService(ApplicationDbContext context)
 {
     public bool CheckUserAccessRightsRead(long ProjectId, ApplicationUser user)
     {
-        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user!.Id && a.ReadAccess == true);
+        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user.Id && a.ReadAccess == true);
     }
 
     public bool CheckUserAccessRightsWrite(long ProjectId, ApplicationUser user)
     {
-        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user!.Id && a.WriteAccess == true);
+        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user.Id && a.WriteAccess == true);
     }
 
     public bool CheckUserAccessRightsManage(long ProjectId, ApplicationUser user)
     {
-        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user!.Id && a.Manage == true);
+        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user.Id && a.Manage == true);
     }
 
     public bool CheckUserAccessRightsOwner(long ProjectId, ApplicationUser user)
     {
-        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user!.Id && a.Owner == true);
+        return context.AccessControls.Any(a => a.Project.Id == ProjectId && a.Member.Id == user.Id && a.Owner == true);
     }
 }
