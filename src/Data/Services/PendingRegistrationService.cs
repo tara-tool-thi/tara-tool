2408a70f248dc4201c23f814b221b49bdae158c0
Emsiiia <emborido@protonmail.com>
2026-05-20 15:54:52 +0200

    Id wird nach erfolgreicher Registrierung automatisch aus der DB gelöscht - Diana, Emily

--- src/Data/Services/PendingRegistrationService.cs
+++ src/Data/Services/PendingRegistrationService.cs
@@ -51,11 +51,11 @@ public class PendingRegistrationService(
     }
     */
 
-    public async Task Delete(string email)
+    public async Task Delete(string id)
     {
         using ApplicationDbContext context = await contextFactory.CreateDbContextAsync();
 
-        PendingRegistration? pendingRegistration = await context.PendingRegistrations.FirstOrDefaultAsync(p => p.Email == email);
+        PendingRegistration? pendingRegistration = await context.PendingRegistrations.FirstOrDefaultAsync(p => p.Id == id);
 
         if (pendingRegistration is null)
         {
@@ -65,6 +65,8 @@ public class PendingRegistrationService(
         context.PendingRegistrations.Remove(pendingRegistration);
         await context.SaveChangesAsync();
     }
+
+
 
     public async Task<List<PendingRegistration>> GetAllPendingRegistrations()
     {
