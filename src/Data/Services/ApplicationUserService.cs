b26051060671a067c82420ed0a248a215b4fbe7e
Aki <aki.kaede@proton.me>
2026-05-20 15:08:06 +0200

    Neue Fehlermeldungen, Logik initialer User fertig, allgemeine Verbesserungen - Emily, Diana

---
+++ src/Data/Services/ApplicationUserService.cs
@@ -0,0 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using tara_tool.Data;
+using tara_tool.Data.Tables;
+
+public class ApplicationUserService(IDbContextFactory<ApplicationDbContext> contextFactory)
+{
+    public async Task<bool> CheckEmailDuplicate(string email)
+    {
+        using ApplicationDbContext context = await contextFactory.CreateDbContextAsync();
+
+        return await context.ApplicationUsers.AnyAsync(p => p.Email == email);
+    }
+
+    public async Task<bool> CheckNameOfUserDuplicate(string username)
+    {
+        using ApplicationDbContext context = await contextFactory.CreateDbContextAsync();
+
+        return await context.ApplicationUsers.AnyAsync(p => p.NameOfUser == username);
+    }
+}
