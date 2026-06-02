6a2c3dd649364be3e54b6e478a3f11ffab7cef97
my0tis <86400600+my0tis@users.noreply.github.com>
2026-06-02 19:21:15 +0200

    Feature/user management2 (#157)
    Co-authored-by: nhixhng <hoanynhi4@gmail.com>
    Co-authored-by: Lea <144854499+ea-ooe@users.noreply.github.com>

--- src/Data/Services/ApplicationUserService.cs
+++ src/Data/Services/ApplicationUserService.cs
@@ -1,8 +1,16 @@
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
 using tara_tool.Data;
+using tara_tool.Data.Tables;
 
 public class ApplicationUserService(IDbContextFactory<ApplicationDbContext> contextFactory)
 {
+    public async Task<List<ApplicationUser>> GetAllUsers()
+    {
+        using ApplicationDbContext context = await contextFactory.CreateDbContextAsync();
+        return await context.ApplicationUsers.ToListAsync(); //Alle Benutzer aus der Datenbank, Tabelle A.U abrufen
+    }
+
     public async Task<bool> CheckEmailDuplicate(string email)
     {
         using ApplicationDbContext context = await contextFactory.CreateDbContextAsync();
