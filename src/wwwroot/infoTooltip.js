9a1a228aefa0c8a9ab9d6508e6801c378e4b2c76
shlzwrd <shlzwrd@gmail.com.>
2026-06-22 16:02:33 +0200

    FluentTooltip cannot open bottom-right, Using CSS hover panels styled with Fluent UI custom properties instead

--- src/wwwroot/infoTooltip.js
+++ src/wwwroot/infoTooltip.js
@@ -1,8 +1,1 @@
-window.patchInfoTooltip = function (anchorId) {
-    const tooltip = document.querySelector('fluent-tooltip[anchor="' + anchorId + '"]');
-    if (!tooltip) return;
-    const region = tooltip.shadowRoot && tooltip.shadowRoot.querySelector('fluent-anchored-region');
-    if (!region) return;
-    region.horizontalDefaultPosition = 'end';
-    region.horizontalPositioningMode = 'dynamic';
-};
+// Info tooltip positioning is handled via CSS (.info-help-container / .info-help-panel in app.css)
