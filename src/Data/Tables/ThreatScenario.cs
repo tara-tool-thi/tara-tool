73c76450af92374f07d924a78038d3228f80ab8a
Adrian <62514017+hyiev1@users.noreply.github.com>
2026-06-24 14:33:06 +0200

    Fix/DS attack path number (#226)
    Co-authored-by: Adrian <hyiev1@users.noreply.github.com>

--- src/Data/Tables/ThreatScenario.cs
+++ src/Data/Tables/ThreatScenario.cs
@@ -10,7 +10,6 @@ public class ThreatScenario
     public DamageScenario? DamageScenarios { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Stride StrideCategorie { get; set; }
     public long RiskValue { get; set; }
     public virtual ICollection<AttackPath> AttackPaths { get; set; } = [];
 }
---
+++ src/Migrations/20260624121824_ThreatScenario-RemoveSTRIDE.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace tara_tool.Migrations
+{
+    /// <inheritdoc />
+    public partial class ThreatScenarioRemoveSTRIDE : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StrideCategorie",
+                table: "ThreatScenarios");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "StrideCategorie",
+                table: "ThreatScenarios",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
