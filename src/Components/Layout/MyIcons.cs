d9933aaa04794f4ff7da1f2998727550e67673ca
Aki <aki.kaede@proton.me>
2026-06-24 15:46:00 +0200

    schonmal gut angefangen

---
+++ src/Components/Layout/MyIcons.cs
@@ -0,0 +1,7 @@
+using Microsoft.FluentUI.AspNetCore.Components;
+using Microsoft.FluentUI.AspNetCore.Components.Icons;
+
+public static class ThiaraIcons
+{
+    public class Logo: Icon { public Logo() : base("Logo", IconVariant.Regular, IconSize.Size20, "<svg width=\"20\" height=\"19\" viewBox=\"0 0 20 19\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\"><path d=\"M 109.51392,297.97462 76.307891,297.31489\" fill=\"#ffffff\" /></svg>") { } }
+}
